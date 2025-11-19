2baaece4191b3229a4584a770c14a96cdf249644
floronoa <f.sertvidal@yahoo.com>
2025-11-19 13:31:39 +0100

    debut insertion

--- cebd-jo-2025-master/utils/excel_extractor.py
+++ cebd-jo-2025-master/utils/excel_extractor.py
@@ -6,6 +6,9 @@ from sqlite3 import IntegrityError
 
 
 def read_excel_file_V0(data: sqlite3.Connection, file):
+
+    # Pays
+
     # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
     # pour construire uniformement la requête
     df_sportifs = pandas.read_excel(
@@ -15,13 +18,52 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
     cursor = data.cursor()
     for ix, row in df_sportifs.iterrows():
         try:
-            query = "insert into LesSportifs values ({},'{}','{}','{}','{}','{}',{})".format(
-                row['numSp'], row['nomSp'], row['prenomSp'], row['pays'], row['categorieSp'], row['dateNaisSp'], row['numEq'])
+            query = "insert into Pays values ('{}')".format(
+                row['pays'])
             # On affiche la requête pour comprendre la construction. A enlever une fois compris.
             print(query)
             cursor.execute(query)
         except IntegrityError as err:
             print(err)
+
+    # Discipline
+
+    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
+    # pour construire uniformement la requête
+    df_sportifs = pandas.read_excel(
+        file, sheet_name='LesEpreuves', dtype=str)
+    df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
+
+    cursor = data.cursor()
+    for ix, row in df_sportifs.iterrows():
+        try:
+            query = "insert into Discipline values ('{}')".format(row['nomDi'])
+            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
+            print(query)
+            cursor.execute(query)
+        except IntegrityError as err:
+            print(err)
+
+    # LesSportifs
+
+    # Lecture de l'onglet du fichier excel LesEpreuves, en interprétant toutes les colonnes comme des strings
+    # pour construire uniformement la requête
+    df_sportifs = pandas.read_excel(
+        file, sheet_name='LesSportifsEQ', dtype=str)
+    df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
+
+    cursor = data.cursor()
+    for ix, row in df_sportifs.iterrows():
+        try:
+            query = "insert into LesSportifs values ('{}','{}','{}','{}','{}','{}')".format(
+                row['numSp'], row['nomSp'], row['prenomSp'], row['pays'], row['categorieSp'], row['dateNaisSp'])
+            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
+            print(query)
+            cursor.execute(query)
+        except IntegrityError as err:
+            print(err)
+
+    # LesEpreuves
 
     # Lecture de l'onglet LesEpreuves du fichier excel, en interprétant toutes les colonnes comme des string
     # pour construire uniformement la requête
@@ -31,7 +73,7 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
     cursor = data.cursor()
     for ix, row in df_epreuves.iterrows():
         try:
-            query = "insert into LesEpreuves values ({},'{}','{}','{}','{}',{},".format(
+            query = "insert into LesEpreuves values ('{}','{}','{}','{}','{}','{}',".format(
                 row['numEp'], row['nomEp'], row['formeEp'], row['nomDi'], row['categorieEp'], row['nbSportifsEp'])
 
             if row['dateEp'] != 'null':
@@ -43,3 +85,78 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
             cursor.execute(query)
         except IntegrityError as err:
             print(f"{err} : \n{row}")
+
+    # Participe
+
+    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
+    # pour construire uniformement la requête
+    df_sportifs = pandas.read_excel(
+        file, sheet_name='LesInscriptions', dtype=str)
+    df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
+
+    cursor = data.cursor()
+    for ix, row in df_sportifs.iterrows():
+        try:
+            query = "insert into Participe values ('{}','{}')".format(
+                row['numIn'], row['numEp'])
+            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
+            print(query)
+            cursor.execute(query)
+        except IntegrityError as err:
+            print(err)
+
+    # Equipe
+
+        # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
+        # pour construire uniformement la requête
+        df_sportifs = pandas.read_excel(
+            file, sheet_name='LesSportifsEQ', dtype=str)
+        df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
+
+        cursor = data.cursor()
+        for ix, row in df_sportifs.iterrows():
+            try:
+                query = "insert into Equipe values ('{}')".format(
+                    row['numEq'])
+                # On affiche la requête pour comprendre la construction. A enlever une fois compris.
+                print(query)
+                cursor.execute(query)
+            except IntegrityError as err:
+                print(err)
+
+    # Enroler
+
+    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
+    # pour construire uniformement la requête
+    df_sportifs = pandas.read_excel(
+        file, sheet_name='LesSportifsEQ', dtype=str)
+    df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
+
+    cursor = data.cursor()
+    for ix, row in df_sportifs.iterrows():
+        try:
+            query = "insert into Enroler values ('{}','{}')".format(
+                row['numSp'], row['numEq'])
+            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
+            print(query)
+            cursor.execute(query)
+        except IntegrityError as err:
+            print(err)
+
+    # Resultat
+    # Lecture de l'onglet du fichier excel LesResultats, en interprétant toutes les colonnes comme des strings
+    # pour construire uniformement la requête
+    df_sportifs = pandas.read_excel(
+        file, sheet_name='LesResultats', dtype=str)
+    df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
+
+    cursor = data.cursor()
+    for ix, row in df_sportifs.iterrows():
+        try:
+            query = "insert into Resultat values ('{}','{}','{}','{}','{}','{}','{}')".format(
+                row['numEp'], row['gold'], row['silver'], row['bronze'], row['gold'], row['silver'], row['bronze'])
+            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
+            print(query)
+            cursor.execute(query)
+        except IntegrityError as err:
+            print(err)
