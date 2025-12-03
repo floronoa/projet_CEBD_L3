33dff16feff496681483f91ea3339783774c9b97
floronoa <f.sertvidal@yahoo.com>
2025-12-03 10:23:07 +0100

    pofinage code

--- cebd-jo-2025-master/actions/database_functions.py
+++ cebd-jo-2025-master/actions/database_functions.py
@@ -4,13 +4,15 @@ from utils import excel_extractor
 from sqlite3 import IntegrityError
 
 # Fonction permettant de créer la base de données
+
+
 def database_create(data):
     print("\nCréation de la base de données")
     try:
         # On exécute les requêtes du fichier de création
         db.updateDBfile(data, "data/v0_createDB.sql")
 
-        db.updateDBfile(data, "data/triggers.sql",True)
+        db.updateDBfile(data, "data/triggers.sql", True)
     except Exception as e:
         # En cas d'erreur, on affiche un message
         print("L'erreur suivante s'est produite pendant lors de la création de la base : " + repr(e) + ".")
@@ -20,6 +22,8 @@ def database_create(data):
         data.commit()
 
 # Fonction permettant d'insérer les données dans la base
+
+
 def database_insert(data):
     print("\nInsertion des données dans la base.")
     try:
@@ -27,13 +31,16 @@ def database_insert(data):
         excel_extractor.read_excel_file_V0(data, "data/LesJO.xlsx")
     except Exception as e:
         # En cas d'erreur, on affiche un message
-        print("L'erreur suivante s'est produite lors de l'insertion des données : " + repr(e) + ".", file=sys.stderr)
+        print("L'erreur suivante s'est produite lors de l'insertion des données : " +
+              repr(e) + ".", file=sys.stderr)
     else:
         # Si tout s'est bien passé, on affiche le message de succès et on commit
         print("Un jeu de test a été inséré dans la base avec succès.")
         data.commit()
 
 # Fonction permettant de supprimer la base de données
+
+
 def database_delete(data):
     print("\nSuppression de la base de données.")
     try:
@@ -46,79 +53,98 @@ def database_delete(data):
         # Si tout s'est bien passé, on affiche le message de succès (le commit est automatique pour un DROP TABLE)
         print("La base de données a été supprimée avec succès.")
 
-# Fonction permettant de supprimer la base de données
+# Fonction permettant de teste les triggers et les contrainte on cascade
+
+
 def database_test(data):
     print("\nLancement des jeux de tests.")
     print("\n---------------------------------\n")
-    # test trigger categorie_ep_sp
+
+    # Test trigger categorie_ep_sp
     cursor = data.cursor()
-    print("test categorie_ep_sp")
-    try :
+    print("Test categorie_ep_sp")
+    print("INSERT INTO Participe VALUES(1016, 3)")
+    try:
         cursor.execute("""INSERT INTO Participe VALUES(1016, 3)""")
-        cursor.execute("""INSERT INTO Participe VALUES(1017, 4)""")
-        cursor.execute("""INSERT INTO Participe VALUES(1016, 1)""")
-        cursor.execute("""INSERT INTO Participe VALUES(1017, 2)""")
-
-        cursor.execute("SELECT * FROM Participe WHERE numSp = 1016")
-        rows = cursor.fetchall()
-        print("Participe numSp=1016:")
-        for i in rows:
-            print(i)
-
-        cursor.execute("SELECT * FROM Participe WHERE numSp = 1017")
-        rows = cursor.fetchall()
-        print("Participe numSp=1017:")
-        for i in rows:
-            print(i)
+        print("Insertion reussie")
 
     except IntegrityError as err:
-            print("TRIGGER : categorie_ep_sp")
+        print("TRIGGER : categorie_ep_sp")
 
-    # test trigger pays_eq
+    print("INSERT INTO Participe VALUES(1017, 4)")
+    try:
+
+        cursor.execute("""INSERT INTO Participe VALUES(1017, 4)""")
+        print("Insertion reussie")
+
+    except IntegrityError as err:
+        print("TRIGGER : categorie_ep_sp")
+
+    print("INSERT INTO Participe VALUES(1016, 1)")
+    try:
+
+        cursor.execute("""INSERT INTO Participe VALUES(1016, 1)""")
+        print("Insertion reussie (voulu epreuve mixte)")
+
+    except IntegrityError as err:
+        print("TRIGGER : categorie_ep_sp")
+
+    print("INSERT INTO Participe VALUES(1016, 3)")
+    try:
+        cursor.execute("""INSERT INTO Participe VALUES(1017, 2)""")
+        print("Insertion reussie (voulu epreuve mixte)")
+
+    except IntegrityError as err:
+        print("TRIGGER : categorie_ep_sp")
+
+    # Test trigger pays_eq
     print("\n---------------------------------\n")
-    print("test pays_eq")
-    try :
+    print("Test pays_eq")
+
+    print("INSERT INTO Enroler VALUES(1003, 30)")
+    try:
+
         cursor.execute("""INSERT INTO Enroler VALUES(1003, 30)""")
         cursor.execute("SELECT * FROM Enroler WHERE numSp = 1003")
-        """rows = cursor.fetchall()
-        print("Enroler numSp=1003:")
-        for i in rows:
-            print(i) """
-
-    except IntegrityError as err:
-            print("TRIGGER : pays_eq")
-    # test trigger date_sp_ep
-    print("\n---------------------------------\n")
-    print("test  date_sp_ep")
-    try :
-        cursor.execute( """INSERT INTO LesSportifs VALUES(1500, 'Jean','Patrick','France','masculin','2026-01-01')""")
-        cursor.execute("SELECT * FROM LesSportifs WHERE numSp = 1500")
         rows = cursor.fetchall()
-        print("Sportif numSp=1500:")
+        print("Enroler numSp=1003:")
         for i in rows:
             print(i)
 
+    except IntegrityError as err:
+        print("TRIGGER : pays_eq")
+
+    # Test trigger date_sp_ep
+    print("\n---------------------------------\n")
+    print("Test  date_sp_ep")
+    print("INSERT INTO LesSportifs VALUES(1500, 'Jean','Patrick','France','masculin','2026-01-01')")
+    cursor.execute(
+        """INSERT INTO LesSportifs VALUES(1500, 'Jean','Patrick','France','masculin','2026-01-01')""")
+
+    print("INSERT INTO Participe VALUES(1500, 1)")
+    try:
         cursor.execute("""INSERT INTO Participe VALUES(1500, 1)""")
         cursor.execute("SELECT * FROM Participe WHERE numSp = 1500")
         rows = cursor.fetchall()
-       # print("Participe numSp=1500:")
         for i in rows:
             print(i)
-    except IntegrityError as err:
-            print("TRIGGER : date_sp_ep")
 
-    #test trigger Nb_Sp_Eq
+    except IntegrityError as err:
+        print("TRIGGER : date_sp_ep")
+
+    # Test trigger Nb_Sp_Eq
     print("\n---------------------------------\n")
-    print("test Nb_Sp_Eq")
-    try : 
+    print("Test Nb_Sp_Eq")
+    print("INSERT INTO Equipe VALUES(95)")
+    print("INSERT INTO Enroler VALUES(1500, 95)")
+    print("INSERT INTO Enroler VALUES(1002, 95)")
+
+    print("DELETE FROM Enroler WHERE numSp = 1002")
+    try:
         cursor.execute("""INSERT INTO Equipe VALUES(95)""")
         cursor.execute("""INSERT INTO Enroler VALUES(1500, 95)""")
         cursor.execute("""INSERT INTO Enroler VALUES(1002, 95)""")
-        cursor.execute("SELECT * FROM Enroler WHERE numEq = 95")
-        rows = cursor.fetchall()
-       # print(" AVANT DELETE Enroler numEp = 95:")
-        for i in rows:
-            print(i)
+
         cursor.execute("""DELETE FROM Enroler WHERE numSp = 1002""")
 
         cursor.execute("SELECT * FROM Enroler WHERE numEq = 95")
@@ -127,33 +153,37 @@ def database_test(data):
         for i in rows:
             print(i)
     except IntegrityError as err:
-            print("TRIGGER : Nb_Sp_Eq")
+        print("TRIGGER : Nb_Sp_Eq")
 
-    #test trigger NbParticipant
+    # Test trigger NbParticipant
     print("\n---------------------------------\n")
-    print("test Nb_Participant")
-    try : 
-        cursor.execute("""INSERT INTO LesEpreuves VALUES(50, 'Batman', 'individuelle', 'Ski alpin', 'mixte', NULL, '2020-04-06 00:00:00')""")
+    print("Test Nb_Participant")
+    print(
+        """INSERT INTO LesEpreuves VALUES(50, 'Batman', 'individuelle', 'Ski alpin', 'mixte', NULL, '2020-04-06 00:00:00')""")
+    print("""INSERT INTO Participe VALUES(1017, 50)""")
+    print("""INSERT INTO Participe VALUES(1016, 50)""")
+    print(
+        """INSERT INTO Resultat VALUES(50, 1017,1016, 1017, NULL, NULL, NULL)""")
+
+    try:
+        cursor.execute(
+            """INSERT INTO LesEpreuves VALUES(50, 'Batman', 'individuelle', 'Ski alpin', 'mixte', NULL, '2020-04-06 00:00:00')""")
         cursor.execute("""INSERT INTO Participe VALUES(1017, 50)""")
         cursor.execute("""INSERT INTO Participe VALUES(1016, 50)""")
-        cursor.execute("SELECT * FROM Participe WHERE numEp = 50")
-        rows = cursor.fetchall()
-        #print(" AVANT INSERT Participe numEp = 50:")
-        for i in rows:
-            print(i)
-        cursor.execute("""INSERT INTO Resultat VALUES(50, 1017,1016, 1017, NULL, NULL, NULL)""")
+        cursor.execute(
+            """INSERT INTO Resultat VALUES(50, 1017,1016, 1017, NULL, NULL, NULL)""")
 
         cursor.execute("SELECT * FROM Participe WHERE numEp = 50")
         rows = cursor.fetchall()
-       # print("APRES INSERT Participe numEp = 50:")
         for i in rows:
             print(i)
     except IntegrityError as err:
-            print("TRIGGER : Nb_Participant")
+        print("TRIGGER : Nb_Participant")
 
     print("\n---------------------------------\n")
-    print("teste delete on cascade")
-    cursor.execute("SELECT * FROM LesSportifs WHERE pays = 'Japon' AND numSp = 1192")
+    print("Test delete on cascade")
+    cursor.execute(
+        "SELECT * FROM LesSportifs WHERE pays = 'Japon' AND numSp = 1192")
     rows = cursor.fetchall()
     print(" AVANT DELETE JAPON SPORTIFS ")
     for i in rows:
@@ -172,7 +202,8 @@ def database_test(data):
         print(i)
     print("\n")
     cursor.execute("""DELETE FROM Pays WHERE pays = 'Japon'""")
-    cursor.execute("SELECT * FROM LesSportifs WHERE pays = 'Japon' AND numSp = 1192")
+    cursor.execute(
+        "SELECT * FROM LesSportifs WHERE pays = 'Japon' AND numSp = 1192")
     rows = cursor.fetchall()
     print(" APRES DELETE JAPON SPORTIFS ")
     for i in rows:
@@ -191,42 +222,48 @@ def database_test(data):
         print(i)
     print("\n")
 
-    
     print("\n---------------------------------\n")
 
-    print("\nteste delete on cascade - Discipline\n")
-    cursor.execute("SELECT * FROM Discipline WHERE nomDi = 'Patinage artistique'")
+    print("\nTest delete on cascade - Discipline\n")
+    cursor.execute(
+        "SELECT * FROM Discipline WHERE nomDi = 'Patinage artistique'")
     rows = cursor.fetchall()
     print(" AVANT DELETE Patinage DISCIPLINE ")
     for i in rows:
         print(i)
     print("\n")
-    cursor.execute("SELECT * FROM LesEpreuves WHERE nomDi = 'Patinage artistique'")
+    cursor.execute(
+        "SELECT * FROM LesEpreuves WHERE nomDi = 'Patinage artistique'")
     rows = cursor.fetchall()
     print(" AVANT DELETE Patinage Epreuves ")
     for i in rows:
         print(i)
     print("\n")
-    cursor.execute("SELECT * FROM Resultat WHERE numEp IN (SELECT numEp FROM LesEpreuves WHERE nomDi = 'Patinage artistique')")
+    cursor.execute(
+        "SELECT * FROM Resultat WHERE numEp IN (SELECT numEp FROM LesEpreuves WHERE nomDi = 'Patinage artistique')")
     rows = cursor.fetchall()
     print(" AVANT DELETE Patinage RESULTATE ")
     for i in rows:
         print(i)
     print("\n")
-    cursor.execute("""DELETE FROM Discipline WHERE nomDi = 'Patinage artistique'""")
-    cursor.execute("SELECT * FROM Discipline WHERE nomDi = 'Patinage artistique'")
+    cursor.execute(
+        """DELETE FROM Discipline WHERE nomDi = 'Patinage artistique'""")
+    cursor.execute(
+        "SELECT * FROM Discipline WHERE nomDi = 'Patinage artistique'")
     rows = cursor.fetchall()
     print(" APRES DELETE Patinage DISCIPLINE ")
     for i in rows:
         print(i)
     print("\n")
-    cursor.execute("SELECT * FROM LesEpreuves WHERE nomDi = 'Patinage artistique'")
+    cursor.execute(
+        "SELECT * FROM LesEpreuves WHERE nomDi = 'Patinage artistique'")
     rows = cursor.fetchall()
     print(" APRES DELETE Patinage Epreuves ")
     for i in rows:
         print(i)
     print("\n")
-    cursor.execute("SELECT * FROM Resultat WHERE numEp IN (SELECT numEp FROM LesEpreuves WHERE nomDi = 'Patinage artistique')")
+    cursor.execute(
+        "SELECT * FROM Resultat WHERE numEp IN (SELECT numEp FROM LesEpreuves WHERE nomDi = 'Patinage artistique')")
     rows = cursor.fetchall()
     print(" APRES DELETE Patinage RESULTATE ")
     for i in rows:
--- cebd-jo-2025-master/utils/excel_extractor.py
+++ cebd-jo-2025-master/utils/excel_extractor.py
@@ -8,9 +8,8 @@ from sqlite3 import IntegrityError
 def read_excel_file_V0(data: sqlite3.Connection, file):
 
     # Pays
-    print("pays")
-    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
-    # pour construire uniformement la requête
+    print("Pays")
+
     df_sportifs = pandas.read_excel(
         file, sheet_name='LesSportifsEQ', dtype=str)
     df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
@@ -20,16 +19,14 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
         try:
             query = "insert or ignore into Pays values ('{}')".format(
                 row['pays'])
-            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            # print(query)
+
             cursor.execute(query)
         except IntegrityError as err:
             print(err)
 
     # Discipline
-    print("discipline")
-    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
-    # pour construire uniformement la requête
+    print("Discipline")
+
     df_epreuves = pandas.read_excel(
         file, sheet_name='LesEpreuves', dtype=str)
     df_epreuves = df_epreuves.where(pandas.notnull(df_epreuves), 'null')
@@ -39,16 +36,14 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
         try:
             query = "insert or ignore into Discipline values ('{}')".format(
                 row['nomDi'])
-            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            # print(query)
+
             cursor.execute(query)
         except IntegrityError as err:
             print(err)
 
     # LesSportifs
-    print("sportifs")
-    # Lecture de l'onglet du fichier excel LesEpreuves, en interprétant toutes les colonnes comme des strings
-    # pour construire uniformement la requête
+    print("LesSportifs")
+
     df_sportifs = pandas.read_excel(
         file, sheet_name='LesSportifsEQ', dtype=str)
     df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
@@ -58,60 +53,52 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
         try:
             query = "insert or ignore into LesSportifs values ({},'{}','{}','{}','{}','{}')".format(
                 row['numSp'], row['nomSp'], row['prenomSp'], row['pays'], row['categorieSp'], row['dateNaisSp'])
-            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            # print(query)
+
             cursor.execute(query)
         except IntegrityError as err:
             print(err)
 
     # LesEpreuves
-    print("epreuve")
-    # Lecture de l'onglet LesEpreuves du fichier excel, en interprétant toutes les colonnes comme des string
-    # pour construire uniformement la requête
+    print("LesEpreuves")
+
     df_epreuves = pandas.read_excel(file, sheet_name='LesEpreuves', dtype=str)
     df_epreuves = df_epreuves.where(pandas.notnull(df_epreuves), 'null')
 
     cursor = data.cursor()
     for ix, row in df_epreuves.iterrows():
         try:
-
+            # Gestion des dates NULL
             if row['dateEp'] != 'null':
                 query = "insert into LesEpreuves values ({},'{}','{}','{}','{}',{},'{}')".format(
                     row['numEp'], row['nomEp'], row['formeEp'], row['nomDi'], row['categorieEp'], row['nbSportifsEp'], row['dateEp'])
-            else:
+            else:  # row['dateEp'] == 'null'
                 query = "insert into LesEpreuves values ({},'{}','{}','{}','{}',{},NULL)".format(
                     row['numEp'], row['nomEp'], row['formeEp'], row['nomDi'], row['categorieEp'], row['nbSportifsEp'])
-            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            # print(query)
+
             cursor.execute(query)
         except IntegrityError as err:
             print(f"{err} : \n{row}")
 
     # Equipe
-    print("equipe")
-    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
-    # pour construire uniformement la requête
+    print("Equipe")
+
     df_sportifs = pandas.read_excel(
         file, sheet_name='LesSportifsEQ', dtype=str)
     df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
     cursor = data.cursor()
     for ix, row in df_sportifs.iterrows():
         try:
-            if row['numEq'] == 'null':
-                pass
-            else:
+            # Gestion des equipes NULL
+            if row['numEq'] != 'null':
                 query = "insert or ignore into Equipe values ({})".format(
                     row['numEq'])
-            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            # print(query)
                 cursor.execute(query)
         except IntegrityError as err:
             print(err)
 
     # Enroler
-    print("enroler")
-    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
-    # pour construire uniformement la requête
+    print("Enroler")
+
     df_sportifs = pandas.read_excel(
         file, sheet_name='LesSportifsEQ', dtype=str)
     df_sportifs = df_sportifs.where(pandas.notnull(df_sportifs), 'null')
@@ -119,21 +106,18 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
     cursor = data.cursor()
     for ix, row in df_sportifs.iterrows():
         try:
-            if row['numEq'] == 'null':
-                pass
-            else:
+            # Gestion des equipes NULL
+            if row['numEq'] != 'null':
                 query = "insert or ignore into Enroler values ({},{})".format(
                     row['numSp'], row['numEq'])
-            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            # print(query)
                 cursor.execute(query)
+
         except IntegrityError as err:
             print(err)
 
     # Participe
-    print("participe")
-    # Lecture de l'onglet du fichier excel LesSportifsEQ, en interprétant toutes les colonnes comme des strings
-    # pour construire uniformement la requête
+    print("Participe")
+
     df_inscriptions = pandas.read_excel(
         file, sheet_name='LesInscriptions', dtype=str)
     df_inscriptions = df_inscriptions.where(
@@ -142,6 +126,8 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
     cursor = data.cursor()
     for ix, row in df_inscriptions.iterrows():
         try:
+            # Verifie si c'est une equipe ou un sportif qui s'inscrit
+            # Si c'est une equipe, on inscrit tout les sportifs de l'equipe
             if int(row['numIn']) < 1000:
                 cursor.execute("""
                 SELECT numSp
@@ -149,29 +135,23 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
                 WHERE numEq = ?;
                 """, [row['numIn']])
                 tab_inscri = {str(i[0]).strip() for i in cursor.fetchall()}
-                # print(tab_inscri)
                 for i in tab_inscri:
                     cursor.execute("insert or ignore into Participe values ({},{})".format(
                         int(i), row['numEp']))
-                # print("if")
-            else:
-                # print("else1")
+            else:  # Inscription du sportif
                 query = "insert or ignore into Participe values ({},{})".format(
                     row['numIn'], row['numEp'])
-                # print("else2")
-            # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            # print(query)
                 cursor.execute(query)
-                # print("else3")
         except IntegrityError as err:
             print(err)
 
     # Resultat
-    print("resultat")
+    print("Resultat")
     df_resultats = pandas.read_excel(
         file, sheet_name='LesResultats', dtype=str)
     df_resultats = df_resultats.where(pandas.notnull(df_resultats), 'null')
 
+    # Recuperation des epreuves individuelle
     cursor = data.cursor()
     cursor.execute("""
     SELECT numEp
@@ -180,23 +160,20 @@ def read_excel_file_V0(data: sqlite3.Connection, file):
     """)
 
     tab_solo = {str(i[0]).strip() for i in cursor.fetchall()}
-    # On affiche la requête pour comprendre la construction. A enlever une fois compris.
 
     for ix, row in df_resultats.iterrows():
         try:
 
+            # Si c'est une epreuve individuelle, il n'y a pas de resultat d'equipe
             if row['numEp'] in tab_solo:
                 query = "insert or ignore into Resultat values ('{}','{}','{}','{}',NULL,NULL,NULL)".format(
                     row['numEp'], row['gold'], row['silver'], row['bronze'])
-                # On affiche la requête pour comprendre la construction. A enlever une fois compris.
-            else:
+
+            else:  # Si c'est une epreuve par equipe, il n'y a pas de resultat individuelle
                 query = "insert or ignore into Resultat values ('{}',NULL,NULL,NULL,'{}','{}','{}')".format(
                     row['numEp'], row['gold'], row['silver'], row['bronze'])
-                # On affiche la requête pour comprendre la construction. A enlever une fois compris.
             cursor.execute(query)
         except IntegrityError as err:
             print(err)
 
-    print("fin insertion du tableau excel")
-    
-    
+    print("Fin insertion du tableau excel")
